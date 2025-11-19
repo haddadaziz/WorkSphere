1ac349d350ff77e1457674533ba5ac9b5c5430eb
hadaz27 <aziz.haddad003@gmail.com>
2025-11-19 15:27:32 +0100

    add : hamburger menu + start date for experiences in form

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ const unassigned_workers_view = document.getElementById("unassigned_workers_view
 const close_form_button = document.getElementById("close_form_button")
 const ajouter_experience_container = document.getElementById("ajouter_experience_container")
 const ajouter_experience_button = document.getElementById("ajouter_experience_button")
+const photo_input_url = document.getElementById('photo_input_url')
+const photo_preview_url = document.getElementById('photo_preview_url')
+const photo_par_defaut = src = "https://www.gravatar.com/avatar/?d=mp&s=128"
+const hide_menu = document.getElementById("hide_menu")
+const close_sidebar_button = document.getElementById("close_sidebar_button")
+const open_sidebar_button = document.getElementById("open_sidebar_button")
+const workspace2D = document.getElementById("workspace2D")
+
+// afficher / cacher le menu
+close_sidebar_button.addEventListener("click", () => {
+    hide_menu.classList.add("hidden")
+    open_sidebar_button.classList.remove("hidden");
+})
+
+open_sidebar_button.addEventListener("click", () => {
+    hide_menu.classList.remove("hidden");
+    open_sidebar_button.classList.add("hidden");
+})
 
 // afficher la page pr ajouter un worker
 go_to_add_worker_form.addEventListener("click", () => {
     ajouter_employe_view.classList.remove("hidden")
     unassigned_workers_view.classList.add("hidden")
 })
-
 close_form_button.addEventListener("click", () => {
     ajouter_employe_view.classList.add("hidden")
     unassigned_workers_view.classList.remove("hidden")
@@ -27,15 +44,23 @@ ajouter_experience_button.addEventListener("click", () => {
 
         <div class="mb-2">
             <label class="text-xs font-medium text-gray-600">Poste</label>
-            <input type="text" name="exp_poste[]" placeholder="Entrez le poste occupé" required
+            <input type="text" name="exp_poste[]" placeholder="Entrez le poste occupé"
                 class="w-full px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
         </div>
         
         <div>
             <label class="text-xs font-medium text-gray-600">Entreprise</label>
-            <input type="text" name="exp_entreprise[]" placeholder="Entrez l'entreprise" required
+            <input type="text" name="exp_entreprise[]" placeholder="Entrez l'entreprise"
                 class="w-full px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
         </div>
+        <div>
+            <label class="text-xs font-medium text-gray-600">Date de Début</label>
+            <input type="date" name="experience_start_date" class="w-full px-2 py-1 border border-gray-300 rounded-md">
+        <div>
+        <div>
+            <label class="text-xs font-medium text-gray-600">Date de Fin</label>
+            <input type="date" name="experience_end_date" class="w-full px-2 py-1 border border-gray-300 rounded-md">
+        <div>
     `
 
     const delete_experience_button = nouvelle_experience_div.querySelector(".delete_experience_button")
@@ -43,4 +68,14 @@ ajouter_experience_button.addEventListener("click", () => {
         nouvelle_experience_div.remove()
     })
     ajouter_experience_container.appendChild(nouvelle_experience_div)
+})
+
+// preview de la photo dans le modal
+photo_input_url.addEventListener("input", () => {
+    const url = photo_input_url.value
+    if (url.trim() !== "") {
+        photo_preview_url.src = url
+    } else {
+        photo_preview_url.src = photo_par_defaut
+    }
 })

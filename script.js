05d31185c600375580ba9863028d801373192c41
hadaz27 <aziz.haddad003@gmail.com>
2025-11-20 15:32:29 +0100

    add : adding experiences in view details popup

--- script.js
+++ script.js
@@ -13,9 +13,26 @@ const open_sidebar_button = document.getElementById("open_sidebar_button")
 const workspace2D = document.getElementById("workspace2D")
 const add_employee_form = document.getElementById("add_employee_form")
 const unassigned_staff_list = document.getElementById("unassigned_staff_list")
+const assign_worker_button = document.getElementById("assign_worker_button")
+const assign_worker_popup = document.getElementById("assign_worker_popup")
+const assign_worker_popup_close_button = document.getElementById("assign_worker_popup_close_button")
+const afficher_employe_popup = document.getElementById("afficher_employe_popup")
+const afficher_employe_popup_close_button = document.getElementById("afficher_employe_popup_close_button")
+const details_employe = document.getElementById("details_employe")
 
-let staff = []
+let staff = JSON.parse(localStorage.getItem("mes_employes")) || []
+afficher_les_employes()
 
+// Afficher / Cacher la popup pour assigner un worker
+assign_worker_button.addEventListener("click", () => {
+    
+    assign_worker_popup.classList.remove("hidden")
+})
+assign_worker_popup_close_button.addEventListener("click", () => {
+    assign_worker_popup.classList.add("hidden")
+})
+
+// formulaire pour ajouter un employé
 add_employee_form.addEventListener("submit", function (e) {
     e.preventDefault()
     const nom = document.getElementById("nom").value
@@ -27,28 +44,49 @@ add_employee_form.addEventListener("submit", function (e) {
     if (photo === "") {
         photo = "https://www.gravatar.com/avatar/?d=mp&s=128"
     }
+    let mes_experiences = []
+    const blocks_experience = ajouter_experience_container.querySelectorAll(".new_employe_experiences")
+
+    blocks_experience.forEach(block => {
+        const poste = block.querySelector("input[name='experience_poste']").value
+        const entreprise = block.querySelector("input[name='experience_entreprise']").value
+        const debut = block.querySelector("input[name='experience_start_date']").value
+        const fin = block.querySelector("input[name='experience_end_date']").value
+
+        const une_experience = {
+            poste: poste,
+            entreprise: entreprise,
+            debut: debut,
+            fin: fin
+        }
+        mes_experiences.push(une_experience)
+    })
 
     const nouvel_employe = {
         nom: nom,
         role: role,
         photo: photo,
         email: email,
-        tel: telephone
+        tel: telephone,
+        experiences: mes_experiences
     }
+
     staff.push(nouvel_employe)
+    localStorage.setItem("mes_employes", JSON.stringify(staff))
     afficher_les_employes()
     add_employee_form.reset()
     photo_preview_url.src = "https://www.gravatar.com/avatar/?d=mp&s=128"
+
     ajouter_employe_view.classList.add("hidden")
     unassigned_workers_view.classList.remove("hidden")
 })
 
 function afficher_les_employes() {
     unassigned_staff_list.innerHTML = ""
-    staff.forEach(employe => {
+    staff.forEach((employe, index) => {
         unassigned_staff_list.innerHTML += `
-            <div class="flex items-center p-3 bg-gray-50 rounded-lg shadow-sm border border-gray-200 mb-2">
-                <img src="${employe.photo}" class="w-12 h-12 rounded-full border border-gray-300 object-cover">
+            <div onclick="voir_details(${index})" class="cursor-pointer flex items-center p-3 bg-gray-50 rounded-lg shadow-sm border border-gray-200 mb-2">
+                <img src="${employe.photo}" class="w-12 h-12 rounded-full border border-gray-300 object-cover border-2 border-blue-500">
                 <div class="ml-3">
                     <p class="text-sm font-semibold text-gray-900">${employe.nom}</p>
                     <p class="text-xs text-gray-600">${employe.role}</p>
@@ -57,6 +95,47 @@ function afficher_les_employes() {
         `
     })
 }
+
+function voir_details(index) {
+    const employe_clique = staff[index]
+    let experiences_html = ""
+    if (employe_clique.experiences?.length > 0) {
+        employe_clique.experiences.forEach(exp => {
+            experiences_html += `
+                <div class="mb-2 border-b border-gray-200 pb-2">
+                    <p class="font-bold text-gray-800">${exp.poste}</p>
+                    <p class="text-blue-600 text-sm">${exp.entreprise}</p>
+                    <p class="text-xs text-gray-500">De ${exp.debut} à ${exp.fin}</p>
+                </div>`
+        })
+    }
+    else {
+        experiences_html += `
+                <div class="mb-2 border-b border-gray-200 pb-2 last:border-0">
+                    <p>Cet employé n'a aucune expérience</p>
+                </div>`
+    }
+    details_employe.innerHTML = `
+        <div class="text-center mb-4">
+            <img src="${employe_clique.photo}" class="w-24 h-24 rounded-full border-2 border-blue-500 mx-auto mb-2 object-cover">
+            <h3 class="text-xl font-bold text-gray-800">${employe_clique.nom}</h3>
+            <p class="text-gray-600 font-medium">${employe_clique.role}</p>
+        </div>
+        <div class="space-y-2 text-sm text-gray-700">
+            <p><strong>Email :</strong> ${employe_clique.email}</p>
+            <p><strong>Téléphone :</strong> ${employe_clique.tel}</p>
+            <hr class="my-3">
+            <h4 class="font-bold mb-2">Expériences :</h4>
+            <p class="text-gray-500 italic">
+            ${experiences_html}
+            </p>
+        </div>
+    `
+    afficher_employe_popup.classList.remove("hidden")
+}
+afficher_employe_popup_close_button.addEventListener("click", () => {
+    afficher_employe_popup.classList.add("hidden")
+})
 
 // afficher / cacher le menu
 close_sidebar_button.addEventListener("click", () => {
@@ -82,7 +161,7 @@ close_form_button.addEventListener("click", () => {
 // formulaire dynamique pr ajouter des experiences
 ajouter_experience_button.addEventListener("click", () => {
     const nouvelle_experience_div = document.createElement("div")
-    nouvelle_experience_div.className = "p-3 border rounded-md bg-gray-50 relative mb-4";
+    nouvelle_experience_div.className = "new_employe_experiences p-3 border rounded-md bg-gray-50 relative mb-4";
     nouvelle_experience_div.innerHTML = `
         <button type="button" class="delete_experience_button absolute top-1 right-2 text-red-400 hover:text-red-600 font-bold text-xl">
             &times
@@ -90,13 +169,13 @@ ajouter_experience_button.addEventListener("click", () => {
 
         <div class="mb-2">
             <label class="text-xs font-medium text-gray-600">Poste</label>
-            <input type="text" name="exp_poste[]" placeholder="Entrez le poste occupé"
+            <input type="text" name="experience_poste" placeholder="Entrez le poste occupé"
                 class="w-full px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
         </div>
         
         <div>
             <label class="text-xs font-medium text-gray-600">Entreprise</label>
-            <input type="text" name="exp_entreprise[]" placeholder="Entrez l'entreprise"
+            <input type="text" name="experience_entreprise" placeholder="Entrez l'entreprise"
                 class="w-full px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
         </div>
         <div>

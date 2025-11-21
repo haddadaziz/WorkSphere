8bd156d46cc3dc9819fac209ba10ad5efe6ade05
hadaz27 <aziz.haddad003@gmail.com>
2025-11-21 16:52:30 +0100

    add : verification si l'employe a l'acces a une salle avec switch case et assigner un employe a une salle

--- script.js
+++ script.js
@@ -13,21 +13,25 @@ const open_sidebar_button = document.getElementById("open_sidebar_button")
 const workspace2D = document.getElementById("workspace2D")
 const add_employee_form = document.getElementById("add_employee_form")
 const unassigned_staff_list = document.getElementById("unassigned_staff_list")
-const assign_worker_button = document.getElementById("assign_worker_button")
+const assign_worker_button = document.querySelectorAll(".assign_worker_button")
 const assign_worker_popup = document.getElementById("assign_worker_popup")
 const assign_worker_popup_close_button = document.getElementById("assign_worker_popup_close_button")
+const assign_worker_popup_staff_list = document.getElementById("assign_worker_popup_staff_list")
 const afficher_employe_popup = document.getElementById("afficher_employe_popup")
 const afficher_employe_popup_close_button = document.getElementById("afficher_employe_popup_close_button")
 const details_employe = document.getElementById("details_employe")
 
 let staff = JSON.parse(localStorage.getItem("mes_employes")) || []
 afficher_les_employes()
+afficher_employes_sur_plan()
 
 // Afficher / Cacher la popup pour assigner un worker
-assign_worker_button.addEventListener("click", () => {
-
-    assign_worker_popup.classList.remove("hidden")
+assign_worker_button.forEach(bouton => {
+    bouton.addEventListener("click", () => {
+        assign_worker_popup.classList.remove("hidden")
+    })
 })
+
 assign_worker_popup_close_button.addEventListener("click", () => {
     assign_worker_popup.classList.add("hidden")
 })
@@ -70,8 +74,8 @@ add_employee_form.addEventListener("submit", function (e) {
         email: email,
         tel: telephone,
         experiences: mes_experiences,
-        localisation : localisation
-        }
+        localisation: localisation
+    }
 
     staff.push(nouvel_employe)
     localStorage.setItem("mes_employes", JSON.stringify(staff))
@@ -86,7 +90,8 @@ add_employee_form.addEventListener("submit", function (e) {
 function afficher_les_employes() {
     unassigned_staff_list.innerHTML = ""
     staff.forEach((employe, index) => {
-        unassigned_staff_list.innerHTML += `
+        if (employe.localisation === "Unassigned") {
+            unassigned_staff_list.innerHTML += `
             <div onclick="voir_details(${index})" class="cursor-pointer flex items-center p-3 bg-gray-50 rounded-lg shadow-sm border border-gray-200 mb-2">
                 <img src="${employe.photo}" class="w-12 h-12 rounded-full border border-gray-300 object-cover border-2 border-blue-500">
                 <div class="ml-3">
@@ -95,6 +100,7 @@ function afficher_les_employes() {
                 </div>
             </div>
         `
+        }
     })
 }
 
@@ -126,7 +132,7 @@ function voir_details(index) {
         <div class="space-y-2 text-sm text-gray-700">
             <p><strong>Email :</strong> ${employe_clique.email}</p>
             <p><strong>Téléphone :</strong> ${employe_clique.tel}</p>
-            <p><strong>Localisation actuelle:</strong> 
+            <p><strong>Localisation actuelle :</strong> 
                 <span>${employe_clique.localisation}</span>
             </p>
             <hr class="my-3">
@@ -209,3 +215,117 @@ photo_input_url.addEventListener("input", () => {
         photo_preview_url.src = photo_par_defaut
     }
 })
+
+function est_autorise(role, salle) {
+    if (role === "manager") {
+        return true
+    }
+    switch (salle) {
+        case "salle_des_serveurs":
+            return role === "technicien_it"
+        case "salle_de_securite":
+            return role === "agent_securite"
+        case "salle_reception":
+            return role === "receptionniste"
+        case "salle_d_archives":
+            return role !== "nettoyage"
+        default:
+            return true
+    }
+}
+
+assign_worker_button.forEach(bouton => {
+    bouton.addEventListener("click", () => {
+        const salle_actuelle = bouton.getAttribute("data-salle")
+        assign_worker_popup_staff_list.innerHTML = ""
+        staff.forEach((employe, index) => {
+            const est_libre = employe.localisation === "Unassigned"
+            const a_le_droit = est_autorise(employe.role, salle_actuelle)
+            if (est_libre && a_le_droit) {
+                assign_worker_popup_staff_list.innerHTML += `
+                    <div class="flex items-center justify-between p-3 bg-gray-50 rounded-lg border mb-2">
+                        <div class="flex items-center">
+                            <img src="${employe.photo}" class="w-10 h-10 rounded-full border border-gray-300 object-cover">
+                            <div class="ml-3">
+                                <p class="text-sm font-bold text-gray-800">${employe.nom}</p>
+                                <p class="text-xs text-gray-500">${employe.role}</p>
+                            </div>
+                        </div>
+                        <button onclick="valider_assignation(${index}, '${salle_actuelle}')" class="bg-blue-600 text-white px-3 py-1 rounded text-xs hover:bg-blue-700">
+                            Assigner
+                        </button>
+                    </div>
+                `
+            }
+        })
+        assign_worker_popup.classList.remove("hidden")
+    })
+})
+
+// Quand on cliquer sur assigner
+function valider_assignation(index, salle) {
+    staff[index].localisation = salle
+    localStorage.setItem("mes_employes", JSON.stringify(staff))
+
+    assign_worker_popup.classList.add("hidden")
+    afficher_les_employes()
+    afficher_employes_sur_plan()
+}
+
+// quand on clique sur retirer
+function retirer_assignation(index) {
+    staff[index].localisation = "Unassigned"
+    localStorage.setItem("mes_employes", JSON.stringify(staff))
+    afficher_les_employes()
+    afficher_employes_sur_plan()
+}
+
+function afficher_employes_sur_plan() {
+    const ids_salles = [
+        "salle_de_conference",
+        "salle_reception",
+        "salle_des_serveurs",
+        "salle_de_securite",
+        "salle_du_personnel",
+        "salle_d_archives"
+    ]
+
+    // vider les salles
+    ids_salles.forEach(id => {
+        const boite = document.getElementById(id);
+        if (boite) {
+            boite.innerHTML = ""
+        }
+    })
+
+    // replacer
+    staff.forEach((employe, index) => {
+        if (employe.localisation !== "Unassigned") {
+            const boite_salle = document.getElementById(employe.localisation);
+            boite_salle.innerHTML += `
+                <div id="carte_employe_assigne" class="bg-white rounded-lg flex flex-col items-center justify-center gap-1 cursor-pointer border border-gray-200 p-1 h-16 w-full"
+                    title="${employe.nom}">
+                    
+                    <img src="${employe.photo}" class="w-8 h-8 rounded-full object-cover border border-gray-100">
+
+                    <div class="flex items-center gap-2 mt-0.5">
+                        <button class="text-gray-400 hover:text-blue-600 transition" title="Déplacer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" class="w-3.5 h-3.5">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+                            </svg>
+                        </button>
+                        
+                        <button onclick="retirer_assignation(${index})" class="text-gray-400 hover:text-red-600 transition" title="Retirer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" class="w-3.5 h-3.5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                `
+        }
+    })
+}
